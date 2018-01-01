44ff9d4fc54146967e65cee909703be682818013
normalsilly <normalsilly@foxmail.com>
2018-01-01 03:10:17 -0500

    ver 1.3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testInspect
 test how to prevent other people inspect my web page
+
+right now can prevent open inspect page by F12 (but not block right click and those with CTRL) and also remove the body DOM temperary when inspecting.
--- static/index.js
+++ static/index.js
@@ -1,9 +1,30 @@
+// Block F12
 $(document).keydown(function(e){
     if(e.which === 123){
        return false;
     }
 });
 
-$(document).bind("contextmenu",function(e) {
-	e.preventDefault();
+// Block right click
+//$(document).bind("contextmenu",function(e) {
+//	e.preventDefault();
+//});
+
+// Hide the specific code (include the real dom) when inspect
+$(document).ready(function() {
+    var currentHtmlContent;
+    var element = new Image();
+    var elementWithHiddenContent = document.querySelector("#hideId");
+    var innerHtml = elementWithHiddenContent.innerHTML;
+
+    element.__defineGetter__("id", function() {
+        currentHtmlContent= "";
+    });
+
+    setInterval(function() {
+        currentHtmlContent= innerHtml;
+        console.log(element);
+        console.clear();
+        elementWithHiddenContent.innerHTML = currentHtmlContent;
+    }, 1000);
 });
